b5a2116026a42103cc4ef707b1f20869bf5b9336
dan850 <dan06982@gmail.com>
2022-09-02 23:57:20 -0500

    CHANGES

--- cookies.js
+++ cookies.js
@@ -1,27 +1,15 @@
+const sixCookies = 6;  //global varible
+
+
 function myFunction() {
 
-	const threeRecipe = {
-		eggs: 0.5,
-		cupsOfSugar : 0.5,
-		cupsOfPb : 0.5
-	  };
+	
+/////////
 
-      console.log("READY TO MAKE " + sixCookies + "PEANUT BUTTER COOKIES IN 3 STEPS?");
-	 
-}
+const threeCookies = 3;
 
-
-
-function myFunction2() {
-   
-  
-	 
- const threeCookies = 3;
-
-const sixCookies = 6;
 const cookieString = "Cookies" //string varible
 
-var TotalIngriedents = 0.0;  //global varible
 
 const sixRecipe = {
 	eggs: 1,
@@ -29,25 +17,24 @@ const sixRecipe = {
 	cupsOfPb : 1
   };
 
- 
- 
-  sixRecipe.eggs= sixRecipe.eggs/2;
+  document.getElementById("demo2").innerHTML = "i'm Hungry!";
+
+  console.log("READY TO MAKE " + sixCookies + "PEANUT BUTTER COOKIES IN 3 STEPS?");
+
 
   console.log(sixRecipe.eggs);
  
  
-  sixRecipe.cupsOfSugar= sixRecipe.cupsOfSugar/2;
  
   console.log(sixRecipe.cupsOfSugar);
  
  
-  sixRecipe.cupsOfPb= sixRecipe.cupsOfPb/2;
  
   console.log(sixRecipe.cupsOfPb);
 
-//Preheat oven to 350 degrees F (175 degrees C). Line baking sheets with parchment paper.
+ 
 
-console.log("READY TO MAKE " + sixCookies + "PEANUT BUTTER COOKIES IN 3 STEPS?");
+
 
 console.log("Preheat oven to " + 350 + " degrees F (175 degrees C). Line baking sheets with parchment paper");
 
@@ -59,9 +46,73 @@ console.log("Drop spoonfuls of dough onto  baking sheet. Bake at 350 degrees F f
 
 
 
+}
+
+
+
+function myFunction2() {
+   
+  const cookieString = "Cookies" //string varible
+
+
+
+  const threeRecipe = {
+		eggs: 0.5,
+		cupsOfSugar : 0.5,
+		cupsOfPb : 0.5
+	  };
+
+    sixRecipe.eggs= sixRecipe.eggs/2;
+
+    console.log(sixRecipe.eggs);
+   
+   
+    sixRecipe.cupsOfSugar= sixRecipe.cupsOfSugar/2;
+   
+    console.log(sixRecipe.cupsOfSugar);
+   
+   
+    sixRecipe.cupsOfPb= sixRecipe.cupsOfPb/2;
+   
+    console.log(sixRecipe.cupsOfPb);
+  
+
+      console.log("DONT WANT AS MANY " +cookieString+ (sixCookies/2) + " DIVIDE THE INGREDIENTS BY 2");
+	 
+
+
+
+
+
+
+
+  console.log(sixRecipe.eggs);
+ 
+ 
+ 
+  console.log(sixRecipe.cupsOfSugar);
+ 
+ 
+ 
+  console.log(sixRecipe.cupsOfPb);
+
+ 
+//Preheat oven to 350 degrees F (175 degrees C). Line baking sheets with parchment paper.
+
+console.log("READY TO MAKE " + sixCookies + "PEANUT BUTTER COOKIES IN 3 STEPS?");
+
+console.log("Preheat oven to " + 350 + " degrees F (175 degrees C). Line baking sheets with parchment paper");
+
+console.log("Combine the peanut butter, white sugar and egg. Mix until smooth.");
+
+console.log("Drop spoonfuls of dough onto  baking sheet. Bake at 350 degrees F for 6 to 8 minutes. ");
+
+ 
+
+
+
 //document.getElementById("demo").innerHTML =
 //person.firstName + " is " + person.age + " years old.";
-
 }
 
 
