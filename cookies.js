e379feb322812ea7ba2450e9d51415f5e513f852
dan850 <dan06982@gmail.com>
2022-09-02 23:19:51 -0500

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,69 @@
+function myFunction() {
+
+	const threeRecipe = {
+		eggs: 0.5,
+		cupsOfSugar : 0.5,
+		cupsOfPb : 0.5
+	  };
+
+      console.log("READY TO MAKE " + sixCookies + "PEANUT BUTTER COOKIES IN 3 STEPS?");
+	 
+}
+
+
+
+function myFunction2() {
+   
+  
+	 
+ const threeCookies = 3;
+
+const sixCookies = 6;
+const cookieString = "Cookies" //string varible
+
+var TotalIngriedents = 0.0;  //global varible
+
+const sixRecipe = {
+	eggs: 1,
+	cupsOfSugar : 1,
+	cupsOfPb : 1
+  };
+
+ 
+ 
+  sixRecipe.eggs= sixRecipe.eggs/2;
+
+  console.log(sixRecipe.eggs);
+ 
+ 
+  sixRecipe.cupsOfSugar= sixRecipe.cupsOfSugar/2;
+ 
+  console.log(sixRecipe.cupsOfSugar);
+ 
+ 
+  sixRecipe.cupsOfPb= sixRecipe.cupsOfPb/2;
+ 
+  console.log(sixRecipe.cupsOfPb);
+
+//Preheat oven to 350 degrees F (175 degrees C). Line baking sheets with parchment paper.
+
+console.log("READY TO MAKE " + sixCookies + "PEANUT BUTTER COOKIES IN 3 STEPS?");
+
+console.log("Preheat oven to " + 350 + " degrees F (175 degrees C). Line baking sheets with parchment paper");
+
+console.log("Combine the peanut butter, white sugar and egg. Mix until smooth.");
+
+console.log("Drop spoonfuls of dough onto  baking sheet. Bake at 350 degrees F for 6 to 8 minutes. ");
+
+
+
+
+
+//document.getElementById("demo").innerHTML =
+//person.firstName + " is " + person.age + " years old.";
+
+}
+
+
+
+
